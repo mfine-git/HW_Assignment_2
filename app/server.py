318bfc64db927bb0628b498ed15bb09127b5f86e
mfine-git <maxim1fine@gmail.com>
2022-05-05 08:18:08 +0300

    task 2: testing build and push to dockerhub

---
+++ app/server.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask import request, Response
+import sys
+
+app = Flask(__name__)
+port = sys.argv[1]
+
+
+@app.route("/")
+def home():
+    return '<h1>Hello from Flask !</h2>'
+
+
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', port=int(port))
